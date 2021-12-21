442d640ac56a739f132e808b20a942a06cd7134b
vocongtruong <kingt3968@gmail.com>
2021-12-21 08:25:44 +0700

    cua hang ban dien may

---
+++ bandienmay/include/footer.php
@@ -0,0 +1,64 @@
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+					Chúng tôi là một trung tâm bán hàng điện máy uy tin trên toàn quốc đảm nảo chất lượng sản phẩm an toàn cho khách hàng, và luôn đêm lại sự hài lòng cho khách hàng khi đến mua sản phẩm tại trung tâm
+				điện máy của chúng tôi.</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 100$</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+		<!-- //footer third section -->
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2018 Electro Store. All rights reserved | Design by
+				<!-- <a href="">Mạng Xã Hội</a> -->
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
